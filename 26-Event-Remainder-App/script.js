7a644ebe546bcb52704b2082b1460032acaff17a
Aditya Kumar <itsadityakr27@gmail.com>
2025-02-21 19:21:54 +0530

    updated

--- 26-Event-Remainder-App/script.js
+++ 26-Event-Remainder-App/script.js
@@ -1,3 +1,5 @@
-const dtElem = document.getElementById(`dtInput`);
+const dateElem = document.getElementById(`dateInput`);
+const timeElem = document.getElementById(`timeInput`);
 const eventElem = document.getElementById(`eventInput`);
 
+let eventArray = [{name,}]
